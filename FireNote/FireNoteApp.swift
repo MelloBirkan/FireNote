82cb63fea5c66bc49f36d22283411aa24433e68f
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2024-09-12 21:29:01 -0300

    Initial Commit

---
+++ FireNote/FireNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireNoteApp.swift
+//  FireNote
+//
+//  Created by Marcello Gonzatto Birkan on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FireNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
