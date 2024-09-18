6495b993113c4d3a90093686f269b945ea2723c5
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2024-09-18 02:10:26 -0300

    `Feat:`Adicionando `Firebase` ao projeto.

---
+++ FireNote/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  FireNote
+//
+//  Created by Marcello Gonzatto Birkan on 18/09/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
--- FireNote/FireNoteApp.swift
+++ FireNote/FireNoteApp.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct FireNoteApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  
     var body: some Scene {
         WindowGroup {
           LoginView()
